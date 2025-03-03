2edfc629ede869130fd098037440506742f985fa
Isaac  Yakubu <engrisaac1234@gmail.com>
2025-03-03 23:00:27 +0100

    Update main.py

--- main.py
+++ main.py
@@ -730,7 +730,7 @@ def dashboard():
             'email': user_data['email'],
             'subscription': {
                 'type': user_data['subscription']['plan'],
-                'startDate': datetime.fromtimestamp(user_data['subscription']['created_at']).strftime('%Y-%m-%d'),
+                'startDate': datetime.fromtimestamp(user_data['subscription']['startDate']).strftime('%Y-%m-%d'),
                 'endDate': datetime.fromtimestamp(user_data['subscription']['endDate']).strftime('%Y-%m-%d') if user_data['subscription']['endDate'] else 'Active',
                 'isActive': user_data['subscription']['isActive']
             }
