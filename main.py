886bfdf12be84ef1c0e1dd07ed92feca9c40d692
Isaac  Yakubu <engrisaac1234@gmail.com>
2025-03-11 15:46:56 +0100

    Update main.py

--- main.py
+++ main.py
@@ -909,3 +909,9 @@ def privacy():
     lang = session.get("lang", "en")
     text = translations_privacy.get(lang, translations_privacy["en"])
     return render_template('privacy.html',text=text)
+
+
+@app.errorhandler(404)
+def page_not_found(error):
+    return render_template('404.html'), 404
+
